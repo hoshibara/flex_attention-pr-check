ff51d7f7ec0c040846b5c1cf6a5078ceb127d625
Xingyuan Li <xingyuan.li@intel.com>
2025-09-11 06:31:47 +0000

    fix

--- reduced-llama3/run.sh
+++ reduced-llama3/run.sh
@@ -21,13 +21,11 @@ TIMESTAMP=$(date '+%Y%m%d-%H%M%S')
 SCRIPT_DIR=$(cd "$(dirname "$0")" && pwd)
 
 # Define the full path to the results directory
-echo ""
-echo "GET RESULTS_DIR: $RESULTS_DIR"
 if [ -z "$RESULTS_DIR" ]; then
   RESULTS_DIR="$SCRIPT_DIR/logs/$TIMESTAMP-reduced-llama3"
   export RESULTS_DIR
 fi
-
+echo -e "\n\nGET RESULTS_DIR: $RESULTS_DIR\n"
 # --- Setup ---
 
 # Create the timestamped directory for results
